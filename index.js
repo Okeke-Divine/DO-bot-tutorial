58237c11010a82d7767fb012014e2ee0df6279c2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-11 17:49:50 +0100

    Updated

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-// const bot = require("puppeteer")
+const bot = require("puppeteer")
 
-const {getRandomItem} = require('./assets/utils/getRandomItem.js')
+const { getRandomItem } = require('./assets/utils/getRandomItem.js')
 
 const botConfiguration = {
     headLess: false
 }
 
-async function runBot(){
+async function runBot() {
+    // get a user agent
     let userAgent = await getRandomItem('./assets/json/list-of-UA.json');
-    console.log(userAgent)
+    console.log("Useragent:", userAgent);
+    // get a user cookie
+    let cookie = await getRandomItem('./assets/json/list-of-cookies.json');
+    console.log("Cookie:", cookie);
+    // get a proxy
+    let proxy = await getRandomItem('./assets/json/list-of-proxies.json');
+    console.log("Proxy:", proxy);
+
+    // const browser = await bot.launch(botConfiguration);
 }
 
 runBot()
