d7611207462314f312f9e1e4f78b2f4104362189
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-11 18:20:30 +0100

    Updated the bot 1.0

--- index.js
+++ index.js
@@ -7,23 +7,38 @@ const botConfiguration = {
 }
 
 async function runBot() {
-    // get a user agent
-    let userAgent = await getRandomItem('./assets/json/list-of-UA.json');
-    console.log("Useragent:", userAgent);
-    // get a user cookie
-    let cookie = await getRandomItem('./assets/json/list-of-cookies.json');
-    console.log("Cookie:", cookie);
-    // get a proxy
-    let proxy = await getRandomItem('./assets/json/list-of-proxies.json');
-    console.log("Proxy:", proxy);
-    // get a link
-    let url = await getRandomItem('./assets/json/list-of-links.json');
-    console.log("Url:", url);
-
     const browser = await puppeteer.launch(botConfiguration);
-    const page = await browser.newPage()
-    page.goto(url);
 
+    try {
+        console.log("============================================================");
+        console.log("::: BEGIN PROCESS :::");
+        console.log("============================================================");
+        // get a user agent
+        let userAgent = await getRandomItem('./assets/json/list-of-UA.json');
+        console.log("Useragent:", userAgent);
+        // get a user cookie
+        let cookie = await getRandomItem('./assets/json/list-of-cookies.json');
+        console.log("Cookie:", cookie);
+        // get a proxy
+        let proxy = await getRandomItem('./assets/json/list-of-proxies.json');
+        console.log("Proxy:", proxy);
+        // get a link
+        let url = await getRandomItem('./assets/json/list-of-links.json');
+        console.log("Url:", url);
+
+        const page = await browser.newPage()
+        page.goto(url);
+    } catch (e) {
+        console.log("An error occured.", e);
+    } finally {
+        setTimeout(function () {
+            browser.close()
+            console.log("============================================================");
+            console.log("::: END PROCESS :::");
+            console.log("============================================================");
+            runBot()
+        }, 30000)
+    }
 
 }
 
